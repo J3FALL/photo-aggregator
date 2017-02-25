1008ea41754d1c5d304e65e5faf76574619b6e28
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-02-25 17:43:31 +0300

    Add Tag and Attachment entities

--- src/domain/domain.go
+++ src/domain/domain.go
@@ -7,7 +7,27 @@ type Photographer struct {
 	Phone   string
 }
 
+type Tag struct {
+	ID   int
+	Name string
+}
+
+type Attachment struct {
+	ID          int
+	Description string
+}
+
 type PhotographerRepository interface {
 	Store(photographer Photographer)
+	FindById(id int)
+}
+
+type TagRepository interface {
+	Store(tag Tag)
+	FindById(id int)
+}
+
+type AttachmentRepository interface {
+	Store(attachment Attachment)
 	FindById(id int)
 }

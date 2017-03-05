267341d2f431b5be787651a4da1ef7593faadb1f
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-03-05 17:02:08 +0300

    Update usecases

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"photo-aggregator/src/domain"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	ph := new(domain.Photographer)
 
 	fmt.Println(ph.ID)
+
+	db, err := sql.Open("postgres", "postgres://postgres:vqislemaro1@localhost/photo?sslmode=disable")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for rows.Next() {
+		var id int
+		var email string
+		errr := rows.Scan(&id, &email)
+		if errr != nil {
+			fmt.Println(errr)
+		}
+		fmt.Println(id, " ", email)
+	}
+
 }
--- src/usecases/usecases.go
+++ src/usecases/usecases.go
@@ -1,1 +1,11 @@
 package usecases
+
+type User struct {
+	Id    int
+	Email string
+}
+
+type UserRepository interface {
+	Store(user User)
+	FindById(id int) User
+}

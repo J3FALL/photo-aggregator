17a52070aa45d60fc1b33a4d75a65bb83dd447d0
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-03-05 20:52:10 +0300

    Update creation script for postgres

--- setup.sql
+++ setup.sql
@@ -1,4 +1,10 @@
 --CREATE DATABASE photo;
+CREATE TABLE photographers (
+  id integer PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  surname varchar(40) NOT NULL,
+  phone varchar(40) NOT NULL
+);
 CREATE TABLE users (
   id integer PRIMARY KEY,
   email varchar(40) NOT NULL

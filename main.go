1ad87592a314805865d74cb3354bd42598cb1436
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-03-23 22:51:56 +0300

    Back to old settings

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"os"
+	"photo-aggregator/src/infrastructure"
+	"photo-aggregator/src/interfaces"
+	"photo-aggregator/src/usecases"
 
 	"github.com/gorilla/mux"
 
@@ -10,30 +13,28 @@ import (
 )
 
 func main() {
-	// dbHandler := infrastructure.NewPgHandler(os.Getenv("DATABASE_URL"))
-	// handlers := make(map[string]interfaces.DbHandler)
-	// handlers["DbUserRepo"] = dbHandler
-	// handlers["DbTagRepo"] = dbHandler
-	// handlers["DbPhotographerRepo"] = dbHandler
-	// handlers["DbAttachmentRepo"] = dbHandler
-	//
-	// photoInteractor := new(usecases.PhotoInteractor)
-	// photoInteractor.UserRepository = interfaces.NewDbUserRepo(handlers)
-	// photoInteractor.PhotographerRepository = interfaces.NewDbPhotographerRepo(handlers)
-	// webServiceHandler := interfaces.WebServiceHandler{}
-	// webServiceHandler.PhotoInteractor = photoInteractor
+	dbHandler := infrastructure.NewPgHandler(os.Getenv("DATABASE_URL"))
+	handlers := make(map[string]interfaces.DbHandler)
+	handlers["DbUserRepo"] = dbHandler
+	handlers["DbTagRepo"] = dbHandler
+	handlers["DbPhotographerRepo"] = dbHandler
+	handlers["DbAttachmentRepo"] = dbHandler
+
+	photoInteractor := new(usecases.PhotoInteractor)
+	photoInteractor.UserRepository = interfaces.NewDbUserRepo(handlers)
+	photoInteractor.PhotographerRepository = interfaces.NewDbPhotographerRepo(handlers)
+	webServiceHandler := interfaces.WebServiceHandler{}
+	webServiceHandler.PhotoInteractor = photoInteractor
 
 	router := mux.NewRouter()
 	router.HandleFunc("/photographers", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "application/json")
-		res.Write([]byte("test"))
-		// webServiceHandler.ShowAllPhotographers(res, req)
+		webServiceHandler.ShowAllPhotographers(res, req)
 	})
 	router.HandleFunc("/photographer/{id}", func(res http.ResponseWriter, req *http.Request) {
 		if req.Method == "GET" {
 			res.Header().Set("Content-Type", "application/json")
-			res.Write([]byte("test"))
-			// webServiceHandler.GetPhotographerById(res, req)
+			webServiceHandler.GetPhotographerById(res, req)
 		}
 	})
 

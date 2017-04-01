b708fe31d3f49c03a8d1ceb8740ebb321d81f28b
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-04-01 16:50:04 +0300

    Change photographers entity fields

--- src/domain/domain.go
+++ src/domain/domain.go
@@ -1,10 +1,11 @@
 package domain
 
 type Photographer struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Phone   string `json:"phone"`
+	ID           int
+	Name         string
+	Surname      string
+	VkURL        string
+	InstagramURL string
 }
 
 type Tag struct {
--- src/usecases/usecases.go
+++ src/usecases/usecases.go
@@ -11,10 +11,11 @@ type User struct {
 }
 
 type Photographer struct {
-	ID      int
-	Name    string
-	Surname string
-	Phone   string
+	ID           int
+	Name         string
+	Surname      string
+	VkURL        string
+	InstagramURL string
 }
 
 type UserRepository interface {
@@ -37,7 +38,7 @@ func (interactor *PhotoInteractor) Photographers() ([]Photographer, error) {
 
 	photographers := []Photographer{}
 	for _, photographer := range photographersTmp {
-		ph := Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, Phone: photographer.Phone}
+		ph := Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, VkURL: photographer.VkURL, InstagramURL: photographer.InstagramURL}
 		photographers = append(photographers, ph)
 	}
 	return photographers, nil
@@ -46,18 +47,19 @@ func (interactor *PhotoInteractor) Photographers() ([]Photographer, error) {
 func (interactor *PhotoInteractor) Photographer(id int) (Photographer, error) {
 	photographerTmp := interactor.PhotographerRepository.FindById(id)
 	fmt.Println(id)
-	photographer := Photographer{ID: photographerTmp.ID, Name: photographerTmp.Name, Surname: photographerTmp.Surname, Phone: photographerTmp.Phone}
+	photographer := Photographer{ID: photographerTmp.ID, Name: photographerTmp.Name, Surname: photographerTmp.Surname, VkURL: photographerTmp.VkURL, InstagramURL: photographerTmp.InstagramURL}
 	return photographer, nil
 }
 
 func (interactor *PhotoInteractor) NewPhotographer(photographer Photographer) {
 	fmt.Println("from usecases")
-	photographerToStore := domain.Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, Phone: photographer.Phone}
+	photographerToStore := domain.Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, VkURL: photographer.VkURL, InstagramURL: photographer.InstagramURL}
 	interactor.PhotographerRepository.Store(photographerToStore)
 	fmt.Println("usecases : good")
 }
 
 func (interactor *PhotoInteractor) UpdatePhotographer(photographer Photographer) bool {
-	photographerToUpdate := domain.Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, Phone: photographer.Phone}
-	return interactor.PhotographerRepository.Update(photographerToUpdate)
+	photographerToUpdate := domain.Photographer{ID: photographer.ID, Name: photographer.Name, Surname: photographer.Surname, VkURL: photographer.VkURL, InstagramURL: photographer.InstagramURL}
+	interactor.PhotographerRepository.Update(photographerToUpdate)
+	return true
 }
